fc945540ed3cfc3e37292772b43ac148cfd10150
Rafael Pinto Biehl <rafaelpbiehl@usp.br>
2026-05-29 11:21:16 -0300

    Buzzer implemention
    Buzzer instead of a motor - Hz and time as placeholders

--- sketch.ino
+++ sketch.ino
@@ -1,6 +1,7 @@
 // Defines pins
 const int pinLDR = 12; 
 const int pinButton = 25;
+const int pinBuzzer = 33;
 int OldValue = LOW;
 
 void setup() {
@@ -18,6 +19,12 @@ if(NewValue == HIGH){
   // Shows values on monitor
   Serial.print("Luminosidade: ");
   Serial.println(valorLuminosidade);
+  if(valorLuminosidade < 2000){ // Higher LUX
+    tone(pinBuzzer, 250, 300); //plays buzzer 25Hz for 0.300 miliseconds
+  }
+  else{ // Lower LUX
+    tone(pinBuzzer, 400, 500);
+  }
 }
   delay(2000);
 }
